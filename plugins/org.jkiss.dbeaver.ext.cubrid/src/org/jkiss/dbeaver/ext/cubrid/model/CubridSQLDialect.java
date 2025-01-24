c54799756598babf015ca6ed04c537091c67f028
rathana-resonest <rathana@ourschool-kep.org>
2025-01-24 12:06:07 +0700

    #35995 close statement after execute.

--- plugins/org.jkiss.dbeaver.ext.cubrid/src/org/jkiss/dbeaver/ext/cubrid/model/CubridSQLDialect.java
+++ plugins/org.jkiss.dbeaver.ext.cubrid/src/org/jkiss/dbeaver/ext/cubrid/model/CubridSQLDialect.java
@@ -36,7 +36,7 @@ public class CubridSQLDialect extends GenericSQLDialect
 {
     public static final String CUBRID_DIALECT_ID = "cubrid";
     private static final Log log = Log.getLog(CubridSQLDialect.class);
-    
+
     private static final String[] CUBRID_KEYWORD = {
             "BIT", "CONNECT_BY_ISCYCLE", "CONNECT_BY_ISLEAF", "CONNECT_BY_ROOT", "CURRENT_DATE", "CURRENT_DATETIME", "CURRENT_TIME", "CURRENT_TIMESTAMP", "CURRENT_USER", "DATA_TYPE",
             "DATABASE", "DATETIME", "DAY_HOUR", "DAY_MILLISECOND", "DAY_MINUTE", "DAY_SECOND", "DISTINCTROW", "DIV", "DO", "DUPLICATE",
@@ -44,7 +44,7 @@ public class CubridSQLDialect extends GenericSQLDialect
             "ROWNUM", "SECOND_MILLISECOND", "SIBLINGS", "SQLCODE", "SQLERROR", "STATISTICS", "SYS_CONNECT_BY_PATH", "SYSDATE",
             "SYSDATETIME", "SYSTIME", "TRUNCATE", "VALUE", "XOR", "YEAR_MONTH"
     };
-    
+
     private static final String[] REMOVE_KEYWORD = {
             "ALIAS", "ALWAYS", "ARRAY", "ASENSITIVE", "ASSIGNMENT", "ASYMMETRIC", "ATOMIC",
             "AUTHORIZATION", "BINARY", "CALLED", "CARDINALITY", "CHAIN", "CHARACTERISTICS",
@@ -54,7 +54,7 @@ public class CubridSQLDialect extends GenericSQLDialect
             "EXCLUDE", "EXCLUDING", "FILTER", "FINAL", "FOLLOWING", "FREE", "FUSION", "GENERATED",
             "GRANTED", "GROUPING", "HIERARCHY", "HOLD", "IMPLEMENTATION", "INCLUDING", "INCREMENT", "INSENSITIVE", "INSTANCE", "INSTANTIABLE",
             "INVOKER", "KEY_TYPE", "LAST_DAY", "LATERAL", "LDB", "LN", "LOCATOR", "LPAD",
-            "MAP", "MATCHED", "MAXVALUE", "MEMBER", "MODIFIES", 
+            "MAP", "MATCHED", "MAXVALUE", "MEMBER", "MODIFIES",
             "MORE", "MUMPS", "NESTING", "NEW", "NOMAXVALUE", "NOMINVALUE", "NORMALIZE", "NORMALIZED",
             "NULLS", "OLD", "OPERATION", "OPERATORS", "OPTIONS", "ORDERING", "ORDINALITY",
             "OTHERS", "OVERLAY", "OVERRIDING", "PARAMETER", "PATH", "PENDANT", "PERCENTILE_CONT",
@@ -84,7 +84,7 @@ public class CubridSQLDialect extends GenericSQLDialect
         source.setSupportMultiSchema(isSupportMultiSchema(session));
         source.setEOLVersion(isEOLVersion(session));
         this.setTracking(session);
-        for(String removeKeyWord: REMOVE_KEYWORD) {
+        for (String removeKeyWord : REMOVE_KEYWORD) {
             this.removeSQLKeyword(removeKeyWord);
         }
         this.addSQLKeywords(Arrays.asList(CUBRID_KEYWORD));
@@ -122,20 +122,24 @@ public class CubridSQLDialect extends GenericSQLDialect
     public int getSchemaUsage() {
         return SQLDialect.USAGE_ALL;
     }
-    
+
     private void setTracking(@NotNull JDBCSession session) {
-    	DBPPreferenceStore store = DBWorkbench.getPlatform().getPreferenceStore();
+        DBPPreferenceStore store = DBWorkbench.getPlatform().getPreferenceStore();
+        JDBCStatement st = null;
+        try {
 
-		try {
+            st = session.createStatement();
 
-		JDBCStatement st =  session.createStatement();
-
-		if(store.getBoolean(CubridConstants.STATISTIC_TRACE))
-			st.execute("SET TRACE ON;");
-		if(!CommonUtils.isEmpty(store.getString(CubridConstants.STATISTIC)))
-			st.execute("set @collect_exec_stats = 1");
+            if (store.getBoolean(CubridConstants.STATISTIC_TRACE))
+                st.execute("SET TRACE ON;");
+            if (!CommonUtils.isEmpty(store.getString(CubridConstants.STATISTIC)))
+                st.execute("set @collect_exec_stats = 1");
         } catch (SQLException e) {
             log.error("Can't set trace", e);
+        } finally {
+            if (st != null) {
+                st.close();
+            }
         }
     }
 }
