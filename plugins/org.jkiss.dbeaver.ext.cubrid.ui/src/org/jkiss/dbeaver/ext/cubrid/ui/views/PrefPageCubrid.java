10e960a8390139cf960c86d4b93323ef11261a77
rathana-resonest <rathana@ourschool-kep.org>
2025-01-26 14:14:32 +0700

    #35995 change pref setting from checkbox to radio

--- plugins/org.jkiss.dbeaver.ext.cubrid.ui/src/org/jkiss/dbeaver/ext/cubrid/ui/views/PrefPageCubrid.java
+++ plugins/org.jkiss.dbeaver.ext.cubrid.ui/src/org/jkiss/dbeaver/ext/cubrid/ui/views/PrefPageCubrid.java
@@ -18,6 +18,7 @@ package org.jkiss.dbeaver.ext.cubrid.ui.views;
 
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
@@ -36,58 +37,68 @@ import org.jkiss.dbeaver.utils.PrefUtils;
 
 public class PrefPageCubrid extends TargetPrefPage
 {
-	public static final String PAGE_ID = "org.jkiss.dbeaver.preferences.cubrid.general"; //$NON-NLS-1$
-	private Button trace;
-	private Button info;
-	private Button allInfo;
-	 public PrefPageCubrid()
-	    {
-	        super();
-	        setPreferenceStore(new PreferenceStoreDelegate(DBWorkbench.getPlatform().getPreferenceStore()));
-	    }
+    public static final String PAGE_ID = "org.jkiss.dbeaver.preferences.cubrid.general"; //$NON-NLS-1$
+    private Button trace;
+    private Button info;
+    private Button allInfo;
 
-	@Override
-	protected boolean hasDataSourceSpecificOptions(DBPDataSourceContainer dsContainer) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    public PrefPageCubrid() {
+        super();
+        setPreferenceStore(new PreferenceStoreDelegate(DBWorkbench.getPlatform().getPreferenceStore()));
+    }
 
-	@Override
-	protected boolean supportsDataSourceSpecificOptions() {
-		return true;
-	}
+    @Override
+    protected boolean hasDataSourceSpecificOptions(DBPDataSourceContainer dsContainer) {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-	@Override
-	protected void loadPreferences(DBPPreferenceStore store) {
-		trace.setSelection(store.getBoolean(CubridConstants.STATISTIC_TRACE));
-		info.setSelection(store.getString(CubridConstants.STATISTIC).equals(CubridConstants.STATISTIC_INFO));
-		allInfo.setSelection(store.getString(CubridConstants.STATISTIC).equals(CubridConstants.STATISTIC_ALL_INFO));
-	}
+    @Override
+    protected boolean supportsDataSourceSpecificOptions() {
+        return true;
+    }
 
-	@Override
-	protected void savePreferences(DBPPreferenceStore store) {
-		store.setValue(CubridConstants.STATISTIC_TRACE, trace.getSelection());
-		PrefUtils.savePreferenceStore(store);
+    @Override
+    protected void loadPreferences(DBPPreferenceStore store) {
+        trace.setSelection(store.getBoolean(CubridConstants.STATISTIC_TRACE));
 
-	}
+        info.setSelection(store.getString(CubridConstants.STATISTIC).equals(CubridConstants.STATISTIC_INFO));
+        allInfo.setSelection(store.getString(CubridConstants.STATISTIC).equals(CubridConstants.STATISTIC_ALL_INFO));
+    }
 
-	@Override
-	protected void clearPreferences(DBPPreferenceStore store) {
-		store.setValue(CubridConstants.STATISTIC_TRACE, false);
-		store.setValue(CubridConstants.STATISTIC, "");
-	}
+    @Override
+    protected void savePreferences(DBPPreferenceStore store) {
 
-	@Override
-	protected String getPropertyPageID() {
+        PrefUtils.savePreferenceStore(store);
 
-		return PAGE_ID;
-	}
+    }
 
-	@Override
-	protected Control createPreferenceContent(Composite parent) {
-		DBPPreferenceStore preferenceStore = DBWorkbench.getPlatform().getPreferenceStore();
-		Composite composite = UIUtils.createPlaceholder(parent, 1);
-		GridLayout gl = new GridLayout(1, false);
+    @Override
+    protected void performDefaults() {
+        super.performDefaults();
+        this.clearPreferences(getTargetPreferenceStore());
+    }
+
+    @Override
+    protected void clearPreferences(DBPPreferenceStore store) {
+        store.setValue(CubridConstants.STATISTIC_TRACE, false);
+        store.setValue(CubridConstants.STATISTIC, "");
+        trace.setSelection(false);
+        info.setSelection(false);
+        allInfo.setSelection(false);
+    }
+
+    @Override
+    protected String getPropertyPageID() {
+
+        return PAGE_ID;
+    }
+
+    @Override
+    protected Control createPreferenceContent(Composite parent) {
+        DBPPreferenceStore preferenceStore = DBWorkbench.getPlatform().getPreferenceStore();
+        Composite composite = UIUtils.createPlaceholder(parent, 1);
+        GridLayout gl = new GridLayout(1, false);
         gl.marginHeight = 10;
         gl.marginWidth = 10;
         composite.setLayout(gl);
@@ -96,37 +107,34 @@ public class PrefPageCubrid extends TargetPrefPage
         {
 
             Group planGroup = UIUtils.createControlGroup(composite, CubridMessages.statistic_group_editor_title, 1, GridData.FILL_HORIZONTAL, 0);
-            info = UIUtils.createCheckbox(planGroup, CubridMessages.statistic_info, false);
-            allInfo = UIUtils.createCheckbox(planGroup, CubridMessages.statistic_all_info, false);
             trace = UIUtils.createCheckbox(planGroup, CubridMessages.statistic_trace_info, false);
 
-            info.addSelectionListener(new SelectionAdapter() {
-            	@Override
-				public void widgetSelected(SelectionEvent e) {
-
-            		if(info.getSelection()) {
-            			allInfo.setSelection(false);
-            			preferenceStore.setValue(CubridConstants.STATISTIC, CubridConstants.STATISTIC_INFO);
-            		}else {
-            			preferenceStore.setValue(CubridConstants.STATISTIC, "");
-            		}
-            	}
+            trace.addSelectionListener(new SelectionAdapter()
+            {
+                @Override
+                public void widgetSelected(SelectionEvent e) {
+                    preferenceStore.setValue(CubridConstants.STATISTIC_TRACE, ((Button) e.widget).getSelection());
+                }
             });
 
-            allInfo.addSelectionListener(new SelectionAdapter() {
-            	@Override
-				public void widgetSelected(SelectionEvent e) {
-            		if(allInfo.getSelection()) {
-            			info.setSelection(false);
-            			preferenceStore.setValue(CubridConstants.STATISTIC, CubridConstants.STATISTIC_ALL_INFO);
-            		}else {
-            			preferenceStore.setValue(CubridConstants.STATISTIC, "");
-            		}
-            	}
-            });
+            SelectionListener radioListener = new SelectionAdapter()
+            {
+                @Override
+                public void widgetSelected(final SelectionEvent e) {
+                    if (e.widget.getData().equals(preferenceStore.getString(CubridConstants.STATISTIC))) {
+                        ((Button) e.widget).setSelection(false);
+                        preferenceStore.setValue(CubridConstants.STATISTIC, "");
+                    } else {
+                        preferenceStore.setValue(CubridConstants.STATISTIC, e.widget.getData().toString());
+                    }
+
+                }
+            };
+            info = UIUtils.createRadioButton(planGroup, CubridMessages.statistic_info, CubridConstants.STATISTIC_INFO, radioListener);
+            allInfo = UIUtils.createRadioButton(planGroup, CubridMessages.statistic_all_info, CubridConstants.STATISTIC_ALL_INFO, radioListener);
 
         }
-		return composite;
-	}
+        return composite;
+    }
 
-	 }
+}
